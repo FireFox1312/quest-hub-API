dc9d8fcfc86c635608a2ec8e998c6ee3804e1381
Fire_Fox <cadupurkott@gmail.com>
2026-07-13 11:14:38 -0300

    chore: migrate server to express framework
    The `server.js` file was migrated to Express, resulting in the following endpoint changes (the old file was removed):
    
    - GET /quests?id=2 => GET /quests/2
    - PUT /quests?id=2 => PUT /quests/2
    - PUT /quests/complete?id=2 => PUT /quests/2/complete
    
    Consequently, the `client.http` file was updated to reflect these changes for testing purposes.

---
+++ src/server.old.js
@@ -0,0 +1,228 @@
+import http from 'node:http';
+
+//Array para armazenamento de Quests
+const quests = [];
+
+//Variável para indexar os elementos do array
+let questId = 0;
+
+
+const PORT = 3000;
+const server = http.createServer((req,res)=>{
+
+    const url = new URL(req.url, `http://${req.headers.host}`); //Guarda a url
+
+    //Endpoint de ping
+    if(req.method === 'GET' && url.pathname === '/ping'){
+
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(JSON.stringify({status: 'OK'}));
+
+    }
+    //Endpoint de listagem do array "quests", com ou sem parâmetro (GET)
+    else if(req.method === 'GET' && url.pathname === '/quests'){
+        
+        
+        //Busca algum parâmetro na URL
+        const idParam = url.searchParams.get('id');
+
+        if(idParam !== null){//Verificação se foi passado algum parâmetro
+
+            //Transformando em inteiro
+            const ID = parseInt(idParam);
+            
+            if(isNaN(ID)){// Verificação de incosistência de parâmetro
+                res.writeHead(400, {'Content-type': 'application/json'});
+                return res.end(JSON.stringify({message: 'ID inválido'}));
+            }
+
+            //Guarda qual quest listar
+            const index = quests.findIndex((quest) => quest.id === ID);
+
+
+            if(index === -1){//Verificação de id inexistênte na lista
+                res.writeHead(404, {'Content-type': 'application/json'});
+                return res.end(JSON.stringify({message: 'Quest não encontrada'}));
+            }
+
+            //Se tudo certo retorna a quest com id correspondente
+            res.writeHead(200, {'Content-type': 'application/json'});
+            return res.end(JSON.stringify(quests[index])); 
+        }
+        //Caso nenhum parâmetro foi passado, retorna a lista completa de quests
+        res.writeHead(200, {'Content-type': 'application/json'});
+        res.end(JSON.stringify(quests));
+
+    }
+    else if(req.method === 'POST' && url.pathname === '/quests'){
+        //Variável para armazenar o corpo da requisição
+        let body = '';
+
+        //Executando cada pedaço de dados que chegam
+        req.on('data', (chunk)=>{
+            body += chunk;
+        });
+
+        req.on('end', ()=>{
+            
+            try{
+                if(!body.trim()){//Breve validação de corpo vazio antes do parse
+                    res.writeHead(400, {'Content-type': 'application/json'});
+                    return res.end(JSON.stringify({message: 'Requisição vazia'}));
+                }
+                
+                let data;
+
+                try{//Teste de Parse bem sucedido
+                    data = JSON.parse(body);
+                } catch(parseError){
+                    //Erro avisando que o body possue erro de formatação
+                    res.writeHead(400, {'Content-type': 'application/json'});
+                    return res.end(JSON.stringify({message: 'JSON mal formatado'}));
+                }
+
+                //Garante que existe, é string e não está vazia após o "trim"
+                if(!data.title || typeof data.title !== 'string' || !data.title.trim()){// Validação do campo obrigatório "title"
+                    res.writeHead(400, {'Content-type': 'application/json'});
+                    return res.end(JSON.stringify({message: 'O campo title é obrigatório e deve ser uma string'}));
+                }
+
+                const newQuest = {//Criação do objeto quest com as chaves do modelo de negócio
+                    id: ++questId ,
+                    title: data.title.trim(),
+                    description: typeof data.description === 'string' ? data.description.trim() : '',
+                    xp: data.xp ? Number(data.xp) : 0,
+                    difficulty: data.difficulty || 'easy',
+                    completed: typeof data.completed === 'boolean' ? data.completed : false //default falso
+                }
+                //Adicionando no fim do array de armazenamento a nova quest
+                quests.push(newQuest);
+
+                res.writeHead(201, {'Content-type': 'application/json'});
+                res.end(JSON.stringify(newQuest));
+
+            }catch(error){//Em caso de erro 
+                
+                res.writeHead(400, {'Content-type': 'application/json'});
+                res.end(JSON.stringify({error: 'Requisição inválida'}));
+
+            }
+        })
+
+
+    }
+    else if(req.method === 'PUT' && url.pathname === '/quests'){
+        
+        //Quarda o ID recebido no parâmetro
+        const idParam = url.searchParams.get('id');
+
+        if(idParam == null){//Verifica se foi passado algum parâmetro
+            res.writeHead(400,{'Content-type': 'application/json'});
+            return res.end(JSON.stringify({message: 'ID não informado'}));
+        }
+        //Passando o ID para inteiro
+        const ID = parseInt(idParam);
+
+        if(isNaN(ID)){//Verifica se o parâmetro é um número
+            res.writeHead(400,{'Content-type': 'application/json'});
+            return res.end(JSON.stringify({message: 'ID inválido'}));
+        }
+
+        //Pega a posição do array
+        const index = quests.findIndex((quest) => quest.id === ID);
+
+        if(index === -1){//Verifica se o id passado existe no array
+            res.writeHead(404,{'Content-type': 'application/json'});
+            return res.end(JSON.stringify({message: 'Quest não encontrada'}));
+        }
+
+        let body = '';
+
+        req.on('data', (chunk)=>{
+            body += chunk;
+        });
+
+        
+        req.on('end', ()=>{
+            try{
+
+                if(!body.trim()){//Breve validação de corpo vazio antes do parse
+                    res.writeHead(400,{'Content-type': 'application/json'});
+                    return res.end(JSON.stringify({message: 'Requisição vazia'}));
+                }
+
+                //Armazena o body em formato JSON
+                const data = JSON.parse(body);
+
+                //Aqui pode ter uma sanitização dos dados para validação do conteúdo
+
+                //Sobreescreve o objeto, mantendo o que não foi alterado e no final forçando o ID original
+                quests[index] = {
+                    ...quests[index],
+                    ...data,
+                    id: quests[index].id
+                }
+
+                res.writeHead(200,{'Content-type': 'application/json'});
+                res.end(JSON.stringify(quests[index]));
+                
+            }
+            catch(error){
+            
+                res.writeHead(400,{'Content-type': 'application/json'});
+                res.end(JSON.stringify({error: 'Requisição inválida'}));
+
+            }
+
+        
+        })
+        
+    }
+    
+    else if(req.method === 'PUT' && url.pathname === '/quests/complete'){
+
+        const idParam = url.searchParams.get('id');
+
+        if(idParam == null){//Verifica se foi passado algum parâmetro
+            res.writeHead(400,{'Content-type': 'application/json'});
+            return res.end(JSON.stringify({message: 'ID não informado'}));
+        }
+
+        const ID = parseInt(idParam);
+
+        if(isNaN(ID)){//Verifica se o parâmetro é um número
+            res.writeHead(400,{'Content-type': 'application/json'});
+            return res.end(JSON.stringify({message: 'ID inválido'}));
+        }
+
+        //Pega a posição do array
+        const index = quests.findIndex((quest) => quest.id === ID);
+
+        if(index === -1){//Verifica se o id passado existe no array
+            res.writeHead(404,{'Content-type': 'application/json'});
+            return res.end(JSON.stringify({message: 'Quest não encontrada'}));
+        }
+
+        if(quests[index].completed){
+            res.writeHead(400,{'Content-type': 'application/json'});
+            return res.end(JSON.stringify({message: 'Quest já completada'}));
+        }
+
+        //Altera a quest para completado
+        quests[index].completed = true;
+
+        res.writeHead(200,{'Content-type': 'application/json'});
+        res.end(JSON.stringify(quests[index]));
+    }
+
+    //Tratamento de rota não encontrada
+    else {
+        res.writeHead(404, {'Content-type': 'application/json'});
+        res.end(JSON.stringify({message: 'Not Found'}));
+    }
+
+});
+
+server.listen(PORT, ()=>{
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+})
