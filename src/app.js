fbb0605aebee6e1d922cf1eadc4c745f1f13b392
Fire_Fox <cadupurkott@gmail.com>
2026-07-16 12:28:30 -0300

    feat: add global logger middleware
    Implemented global middleware (logger.js) to intercept requests and log the date/time, HTTP method, and accessed URL to the terminal. The middleware was registered in app.js before the routes to ensure complete flow tracking.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 //Importação do express e do arquivo de rotas
 import express from 'express';
 import questRoute from './routes/quest-route.js';
+import { logger } from './middlewares/logger.js';
+import { notFound } from './middlewares/notFound.js';
 
 //Objeto que vai conter todos o métodos do express
 const app = express();
 
 //Fará o parse dos dados para JSON
 app.use(express.json());
+
+//Utiliza o logger toda vez antes da requisição passar pela rota
+app.use(logger);
 
 //Rota para o endpoint /quests, que vai usar o router definido no arquivo quest-route.js
 app.use('/quests', questRoute);
@@ -16,9 +21,8 @@ app.get('/ping', (req,res)=>{
     res.status(200).json({status: 'OK'});
 });
 
-//Rota para lidar com erros de rota não encontrada
-app.use((req, res) => {
-    res.status(404).json({message: 'Rota não encontrada'});
-});
+//Se passar por tudo chama o middleware responsável pelo erro de rota não encontrada
+app.use(notFound);
 
+//Exportação do app para ser usado no server.js
 export default app;
