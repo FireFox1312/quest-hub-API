1a43caca46ff3685a363a26c6b87449330e80c50
Fire_Fox <cadupurkott@gmail.com>
2026-07-29 09:58:20 -0300

    feat: add asyncWrapper to controllers removing try-catch blocks
    Created the `asyncWrapper` utility in `utils/` and refactored `quest-controller` to use it, eliminating manual `try-catch` blocks and preparing the routes for Promise-based operations.

--- src/controllers/quest-controller.js
+++ src/controllers/quest-controller.js
@@ -1,3 +1,4 @@
+import { asyncWrapper } from "../utils/async-wrapper.js";
 
 export class QuestController{
 
@@ -10,7 +11,7 @@ export class QuestController{
     }
 
     //Chama o service pra retornar a lista
-    getControll = (req, res) => {
+    getControll = asyncWrapper(async(req, res) => {
         //Guarda os filtros passados na URL
         const filters = req.query;
 
@@ -24,81 +25,63 @@ export class QuestController{
         //Guarda a lista retornada pelo service
         const questList = this.questService.getAll(filters);
         res.status(200).json(questList);
-    }
+    })
 
     //Chama o service pra retornar a quest pelo id tratado
-    searchControll = (req, res, next) => {
+    searchControll = asyncWrapper(async(req, res, next) => {
         //Pega o id da requisição e transforma em número
         const id = parseInt(req.params.id, 10);
 
-        try{
-            //Guarda a quest retornada pelo service
-            const quest = this.questService.getById(id);
-            res.status(200).json(quest);
-        }
-        catch(error){//Caso não exista a quest, repassa o erro para o error handler
-            next(error);
-        }
-    }
+        //Guarda a quest retornada pelo service
+        const quest = this.questService.getById(id);
+        res.status(200).json(quest);
+        
+    })
 
     //Chama o service pra criar a quest com os dados do body
-    postControll = (req, res, next) => {
+    postControll = asyncWrapper(async(req, res, next) => {
         //Quarda as informações do body
         const data = req.body;
 
-        try{
-            //Guarda a quest criada pelo service
-            const newQuest = this.questService.create(data);
-            res.status(201).json(newQuest);
-        }
-        catch(error){//Caso não exista a quest, repassa o erro para o error handler
-            next(error);
-        }
-    }
+        //Guarda a quest criada pelo service
+        const newQuest = this.questService.create(data);
+        res.status(201).json(newQuest);
+        
+    })
 
     //Chama o service pra atualizar a quest com os dados do body
-    putControll = (req, res, next) => {
+    putControll = asyncWrapper(async(req, res, next) => {
         //Pega o id da requisição e transforma em número
         const id = parseInt(req.params.id, 10);
         //Quarda as informações do body
         const data = req.body;
-    
-        try{
-            //Guarda a quest atualizada pelo service
-            const updatedQuest = this.questService.update(id, data);
-            res.status(200).json(updatedQuest);
-        }
-        catch(error){//Caso não exista a quest, repassa o erro para o error handler
-            next(error);
-        }
-    }
+        
+        //Guarda a quest atualizada pelo service
+        const updatedQuest = this.questService.update(id, data);
+        res.status(200).json(updatedQuest);
+        
+    })
 
     //Chama o service pra completar a quest com o id passado
-    completeControll = (req, res, next) => {
+    completeControll = asyncWrapper(async(req, res, next) => {
         //Pega o id da requisição e transforma em número
         const id = parseInt(req.params.id, 10);
-    
-        try{
-            //Guarda a quest completada pelo service
-            const completedQuest = this.questService.complete(id);
-            res.status(200).json(completedQuest);
-        }
-        catch(error){//Caso não exista a quest, repassa o erro para o error handler
-            next(error);
-        }
-    }
+        
+        //Guarda a quest completada pelo service
+        const completedQuest = this.questService.complete(id);
+        res.status(200).json(completedQuest);
+        
+    })
 
-    deleteControll = (req, res, next) => {
+    deleteControll = asyncWrapper(async(req, res, next) => {
         //Pega o id da requisição e transforma em número
         const id = parseInt(req.params.id, 10);
-
-        try{
-            const deletedQuest = this.questService.delete(id);
+       
+        const deletedQuest = this.questService.delete(id);
             
-            res.status(204).send();
-        }
-        catch(error){
-            next(error);
-        }
-    }
+        res.status(204).send();
+    
+    })
+
+    
 }
---
+++ src/utils/async-wrapper.js
@@ -0,0 +1,7 @@
+
+//Uma função que recebe uma função assíncrona e retorna uma nova função que lida com erros de forma adequada.
+export const asyncWrapper = (fn) => {
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch(next);
+    }
+}
