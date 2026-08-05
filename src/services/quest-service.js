1572857cab09de39d5fa4c5ab9d53e0563a7daee
Cadu Purkott <96633955+FireFox1312@users.noreply.github.com>
2026-08-05 12:57:45 -0300

    Revert "Feature/sqlite persistence"

--- src/services/quest-service.js
+++ src/services/quest-service.js
@@ -6,14 +6,14 @@ export class QuestService {
         this.questRepository = questRepository;
     }
     //Retorna todas as quests
-    async getAll(filters) {
-        return await this.questRepository.findAll(filters);
+    getAll(filters) {
+        return this.questRepository.findAll(filters);
     }
 
     //Retorna a quest pelo id
-    async getById(id) {
+    getById(id) {
         //Busca pelo id e guarda a quest
-        const quest =  await this.questRepository.findById(id);
+        const quest = this.questRepository.findById(id);
 
         //Verifica se existe, se não existe lança um erro
         if(!quest) throw new NotFoundError('Quest não encontrada');
@@ -23,37 +23,37 @@ export class QuestService {
     }
 
     //Cria uma quest com as informações do body "data"
-    async create(data) {
+    create(data) {
         //Retorna a quest criada
-        return await this.questRepository.create(data);
+        return this.questRepository.create(data);
     }
 
     //Faz o update de uma quest
-    async update(id, data){
+    update(id, data){
         //Busca a quest pelo id
-        const quest = await this.getById(id);
+        const quest = this.getById(id);
 
         //Retorna a quest atualizada
-        return await this.questRepository.update(quest.id, data);
+        return this.questRepository.update(quest.id, data);
     }
 
     //Altera para completa a quest passada
-    async complete(id){
+    complete(id){
         //Busca a quest pelo id
-        const quest = await this.getById(id);
+        const quest = this.getById(id);
 
         if(quest.completed){//Verifica se já está completada
             throw new ValidationError('Quest já completada');
         }
         //Retorna a quest atualizada
-        return await this.questRepository.update(quest.id, {completed: true});
+        return this.questRepository.update(quest.id, {completed: true});
     }
 
-    async delete(id){
+    delete(id){
         //Busca a quest pelo id
-        const quest = await this.getById(id);
+        const quest = this.getById(id);
 
-        return await this.questRepository.delete(quest.id);
+        return this.questRepository.delete(quest.id);
         
     }
 
