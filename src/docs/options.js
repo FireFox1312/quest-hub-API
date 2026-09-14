fcb09f32641b9e493bbd3bd673b54c522f0512c6
Fire_Fox <cadupurkott@gmail.com>
2026-09-14 12:54:25 -0300

    feat: add scalar ui and customize swagger dx
    - Integrate @scalar/express-api-reference for modern API documentation
    
    - Provide automatic client generation snippets in Scalar UI
    
    - Add custom options (title, css) to legacy Swagger UI

---
+++ src/docs/options.js
@@ -0,0 +1,12 @@
+
+export const swaggerOptions = {
+
+    customSiteTitle: 'Quest Hub API - Documentação',
+    customCss: 
+        `.swagger-ui .topbar {
+            display: none;
+        } 
+        .swagger-ui { 
+        background-color: #f8f9fa; 
+        }`
+}
